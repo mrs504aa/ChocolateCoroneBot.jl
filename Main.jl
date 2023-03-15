569456fc3f071ab7261ac2ef56c58fe38c69762f
Ruosong Mao <mrs504aa@hotmail.com>
2023-03-15 21:48:16 +0800

    Larger time delay.

--- Main.jl
+++ Main.jl
@@ -1,5 +1,12 @@
 using Pkg
 Pkg.activate(".")
+
+using Downloads
+Downloads.DOWNLOADER[] = nothing
+Downloads.EASY_HOOK[] = (easy, info) -> begin
+    Downloads.Curl.setopt(easy, Downloads.Curl.CURLOPT_LOW_SPEED_TIME, 60)
+end
+
 using ChocolateCoroneBot
 
 StartBot()
